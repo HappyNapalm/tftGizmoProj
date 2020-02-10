49a246496fbe45445cdf39213eea106b31e30607
Church <maevestring@gmail.com>
2020-02-09 18:19:54 -0800

    Running into type casting errors.

--- Ornament/code.py
+++ Ornament/code.py
@@ -130,23 +130,23 @@ the board to see the values change. If you're using Mu, open the plotter to see 
 left to see the NeoPixels light up in colors related to the accelerometer! The Circuit Playground
 has an accelerometer in the center that returns (x, y, z) acceleration values. This program uses
 those values to light up the NeoPixels based on those acceleration values."""
-from adafruit_circuitplayground import cp
+# from adafruit_circuitplayground import cp
 
-# Main loop gets x, y and z axis acceleration, prints the values, and turns on
-# red, green and blue, at levels related to the x, y and z values.
-while True:
-    if not cp.switch:
-        # If the switch is to the right, it returns False!
-        print("Slide switch off!")
-        cp.pixels.fill((0, 0, 0))
-        continue
-    else:
-        R = 0
-        G = 0
-        B = 0
-        x, y, z = cp.acceleration
-        print((x, y, z))
-        cp.pixels.fill(((R + abs(int(x))), (G + abs(int(y))), (B + abs(int(z)))))
+# # Main loop gets x, y and z axis acceleration, prints the values, and turns on
+# # red, green and blue, at levels related to the x, y and z values.
+# while True:
+#     if not cp.switch:
+#         # If the switch is to the right, it returns False!
+#         print("Slide switch off!")
+#         cp.pixels.fill((0, 0, 0))
+#         continue
+#     else:
+#         R = 0
+#         G = 0
+#         B = 0
+#         x, y, z = cp.acceleration
+#         print((x, y, z))
+#         cp.pixels.fill(((R + abs(int(x))), (G + abs(int(y))), (B + abs(int(z)))))
 
 # """This example plays a different tone for a duration of 1 second for each button pressed."""
 # from adafruit_circuitplayground import cp
@@ -161,42 +161,57 @@ while True:
 This test will initialize the display using displayio and draw a solid green
 background, a smaller purple rectangle, and some yellow text.
 """
-# import displayio
-# import terminalio
-# from adafruit_display_text import label
-# from adafruit_gizmo import tft_gizmo
+import displayio
+import terminalio
+from adafruit_display_text import label
+from adafruit_gizmo import tft_gizmo
+from adafruit_circuitplayground import cp
 
-# # Create the TFT Gizmo display
-# display = tft_gizmo.TFT_Gizmo()
+# Create the TFT Gizmo display
+display = tft_gizmo.TFT_Gizmo()
 
-# # Make the display context
-# splash = displayio.Group(max_size=10)
-# display.show(splash)
+# Make the display context
+splash = displayio.Group(max_size=10)
+display.show(splash)
 
-# color_bitmap = displayio.Bitmap(240, 240, 1)
-# color_palette = displayio.Palette(1)
-# color_palette[0] = 0x00FF00 # Bright Green
+color_bitmap = displayio.Bitmap(240, 240, 1)
+color_palette = displayio.Palette(1)
+color_palette[0] = 0x00FF00 # Bright Green
 
-# bg_sprite = displayio.TileGrid(color_bitmap,
-#                                pixel_shader=color_palette,
-#                                x=0, y=0)
-# splash.append(bg_sprite)
+bg_sprite = displayio.TileGrid(color_bitmap,
+                               pixel_shader=color_palette,
+                               x=0, y=0)
+splash.append(bg_sprite)
 
-# # Draw a smaller inner rectangle
-# inner_bitmap = displayio.Bitmap(200, 200, 1)
-# inner_palette = displayio.Palette(1)
-# inner_palette[0] = 0xAA0088 # Purple
-# inner_sprite = displayio.TileGrid(inner_bitmap,
-#                                   pixel_shader=inner_palette,
-#                                   x=20, y=20)
-# splash.append(inner_sprite)
+# Draw a smaller inner rectangle
+inner_bitmap = displayio.Bitmap(200, 200, 1)
+inner_palette = displayio.Palette(1)
+inner_palette[0] = 0xAA0088 # Purple
+inner_sprite = displayio.TileGrid(inner_bitmap,
+                                  pixel_shader=inner_palette,
+                                  x=20, y=20)
+splash.append(inner_sprite)
 
-# # Draw a label
-# text_group = displayio.Group(max_size=10, scale=2, x=50, y=120)
-# text = "Hello World!"
-# text_area = label.Label(terminalio.FONT, text=text, color=0xFFFF00)
-# text_group.append(text_area) # Subgroup for text scaling
-# splash.append(text_group)
+# Draw a label
+text_group = displayio.Group(max_size=10, scale=2, x=50, y=120)
+text = "Hello World!"
+text_area = label.Label(terminalio.FONT, text=text, color=0xFFFF00)
+text_group.append(text_area) # Subgroup for text scaling
+splash.append(text_group)
 
-# while True:
-#     pass
+while True:
+    if not cp.switch:
+        print("Slide switch off!")
+        cp.pixels.fill((0, 0, 0))
+        continue
+    else:
+        R = 0
+        G = 0
+        B = 0
+        x, y, z = cp.acceleration
+        print((x, y, z))
+        cp.pixels.fill(((R + abs(int(x))), (G + abs(int(y))), (B + abs(int(z)))))
+        pixel_color_hex = hex(int(str(R + abs(int(x)))))<<4 + hex(int(str(G + abs(int(x)))))<<2 + hex(int(str(B + abs(int(x)))))
+        # print(pixel_color_hex)
+        # inner_palette[0] = pixel_color_hex
+    pass
